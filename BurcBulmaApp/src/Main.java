1b9577292e0aec59d05253d550d56436b787c3a1
Ömer Umutlu <79787687+omerumutluu@users.noreply.github.com>
2022-01-25 18:32:44 +0300

    Add files via upload

---
+++ BurcBulmaApp/src/Main.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int month, day;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Doğduğunuz ay : ");
+        month = scanner.nextInt();
+
+        System.out.print("Doğduğunuz gün : ");
+        day = scanner.nextInt();
+
+        if (month == 1){
+            if (day >= 22)
+                System.out.println("");
+        }
+        else if (month == 2){
+
+        }
+        else if (month == 3){
+
+        }
+        else if (month == 4){
+
+        }
+        else if (month == 5){
+
+        }
+        else if (month == 6){
+
+        }
+        else if (month == 7){
+
+        }
+        else if (month == 8){
+
+        }
+        else if (month == 9){
+
+        }
+        else if (month == 10){
+
+        }
+        else if (month == 11){
+
+        }
+        else if (month == 12){
+
+        }
+    }
+}
